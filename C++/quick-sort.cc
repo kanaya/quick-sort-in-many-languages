15db38278b2b92033e88177e1ee62f0c6e48a76d
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-02 17:22:51 +0900

    Now uses 100 random number vector.

--- C++/quick-sort.cc
+++ C++/quick-sort.cc
@@ -1,18 +1,23 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iterator>
 #include <iostream>
+#include <vector>
 
-int partition(int arr[], int start, int end) {
-	int pivot = arr[start];
-	int count = 0;
+template <typename T>
+size_t partition(std::vector<T> &arr, size_t start, size_t end) {
+	T pivot = arr[start];
+	size_t count = 0;
 
-	for (int i = start + 1; i <= end; ++i) {
+	for (size_t i = start + 1; i <= end; ++i) {
 		if (arr[i] <= pivot)
 			++count;
 	}
 
-	int pivotIndex = start + count;
+	size_t pivotIndex = start + count;
 	std::swap(arr[pivotIndex], arr[start]);
 
-	int i = start, j = end;
+	size_t i = start, j = end;
 
 	while (i < pivotIndex && j > pivotIndex) {
 		while (arr[i] <= pivot) {
@@ -30,19 +35,25 @@ int partition(int arr[], int start, int end) {
 	return pivotIndex;
 }
 
-void quickSort(int arr[], int start, int end) {
+template <typename T>
+void quickSort(std::vector<T> &arr, size_t start, size_t end) {
 	if (start >= end) {
 		return;
 	}
 
-	int p = partition(arr, start, end);
+	size_t p = partition(arr, start, end);
 	quickSort(arr, start, p - 1);
 	quickSort(arr, p + 1, end);
 }
 
 int main() {
-	int arr[] = { 9, 3, 4, 2, 1, 8 };
-	int n = 6;
+	auto gen = []() {
+		return std::rand() % 100;
+	};
+
+	std::vector<int> arr(100);
+	std::generate(begin(arr), end(arr), gen);
+	size_t n = arr.size();
 
 	quickSort(arr, 0, n - 1);
 	for (int i = 0; i < n; ++i) {
