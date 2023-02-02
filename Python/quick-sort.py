19a4ded9e99ad51204e684aa0c5a70298a5c1fcb
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-02 16:47:26 +0900

    Removed keyFn.

--- Python/quick-sort.py
+++ Python/quick-sort.py
@@ -5,12 +5,12 @@ import random
 def median3(x, y, z):
 	return max(min(x, y), min(max(x, y), z))
 
-def partition(seq: MutableSequence[Any], keyFn: Callable[[Any], Any], first: int, last: int):
-	pivot = median3(keyFn(seq[first]), keyFn(seq[first + (last - first) // 2]), keyFn(seq[last]))
+def partition(seq: MutableSequence[Any], first: int, last: int):
+	pivot = median3(seq[first], seq[first + (last - first) // 2], seq[last])
 	while True:
-		while keyFn(seq[first]) < pivot:
+		while seq[first] < pivot:
 			first += 1
-		while pivot < keyFn(seq[last]):
+		while pivot < seq[last]:
 			last -= 1
 		if first >= last:
 			return last + 1
@@ -18,21 +18,18 @@ def partition(seq: MutableSequence[Any], keyFn: Callable[[Any], Any], first: int
 		first += 1
 		last -= 1
 
-def quick_sort(seq: MutableSequence[Any], keyFn: Callable[[Any], Any]):
-	def quick_sort_impl(seq: MutableSequence, keyFn: Callable[[Any], int], first: int, last: int):
+def quick_sort(seq: MutableSequence[Any]):
+	def quick_sort_impl(seq: MutableSequence, first: int, last: int):
 		while first < last:
-			p = partition(seq, keyFn, first, last)
+			p = partition(seq, first, last)
 			if (p - first) < (last - p):
-				quick_sort_impl(seq, keyFn, first, p - 1)
+				quick_sort_impl(seq, first, p - 1)
 				first = p
 			else:
-				quick_sort_impl(seq, keyFn, p, last)
+				quick_sort_impl(seq, p, last)
 				last = p - 1
-	quick_sort_impl(seq, keyFn, 0, len(seq) - 1)
-
-def id(x):
-	return x
+	quick_sort_impl(seq, 0, len(seq) - 1)
 
 a = [random.randrange(100) for x in range(0, 100)]
-quick_sort(a, id)
+quick_sort(a)
 print(a)
