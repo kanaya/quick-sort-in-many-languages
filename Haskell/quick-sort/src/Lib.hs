22ab954857334e30d8d5af07e208093562cf377d
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-02 22:01:42 +0900

    100 random list.

--- Haskell/quick-sort/src/Lib.hs
+++ Haskell/quick-sort/src/Lib.hs
@@ -2,12 +2,17 @@ module Lib
     ( someFunc
     ) where
 
+import System.Random
+
 quickSort :: Ord a => [a] -> [a]
 quickSort [] = []
 quickSort [x] = [x]
 quickSort (x:xs) = quickSort left ++ [x] ++ quickSort right
     where left  = filter (<  x) xs
           right = filter (>= x) xs
+
+generateRandomList :: Int -> IO [Int]
+generateRandomList n = sequence $ replicate n $ randomRIO (1,100::Int)
 
 someFunc :: IO ()
 someFunc = print $ quickSort [1, 8, 7, 6, 2, 5, 9, 4, 0, 3]
