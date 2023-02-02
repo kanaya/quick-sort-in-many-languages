d71ca2415f55dcc9e075df145ba3f71099b4a5f7
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-02 14:48:13 +0900

    The first quick sort in Haskell.

--- Haskell/quick-sort/src/Lib.hs
+++ Haskell/quick-sort/src/Lib.hs
@@ -2,5 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort [x] = [x]
+quickSort (x:xs) = quickSort left ++ [x] ++ quickSort right
+    where left  = filter (<  x) xs
+          right = filter (>= x) xs
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ quickSort [1, 8, 7, 6, 2, 5, 9, 4, 0, 3]
+
