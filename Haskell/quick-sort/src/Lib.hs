65427152ef0370669b0f9a7873a81f0622a237b1
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-03 16:45:25 +0900

    refactor.

--- Haskell/quick-sort/src/Lib.hs
+++ Haskell/quick-sort/src/Lib.hs
@@ -12,7 +12,7 @@ quickSort (x:xs) = quickSort left ++ [x] ++ quickSort right
           right = filter (>= x) xs
 
 generateRandomList :: Int -> IO [Int]
-generateRandomList n = sequence $ replicate n $ randomRIO (1,100::Int)
+generateRandomList n = sequence $ replicate n $ randomRIO (1, 100)
 
 someFunc :: IO ()
 someFunc = (quickSort <$> generateRandomList 100) >>= print
