7aa1ea0b0205bff66dd8572c6b93927d3b6768e1
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-03 16:42:39 +0900

    Refactor.

--- Python/quicksort.py
+++ Python/quicksort.py
@@ -13,18 +13,19 @@ def partition(arr, first, last):
 		first += 1
 		last -= 1
 
-def quick_sort(arr):
-	def quick_sort_impl(arr, first, last):
-		while first < last:
-			p = partition(arr, first, last)
-			if (p - first) < (last - p):
-				quick_sort_impl(arr, first, p - 1)
-				first = p
-			else:
-				quick_sort_impl(arr, p, last)
-				last = p - 1
-	quick_sort_impl(arr, 0, len(arr) - 1)
+def quick_sort(arr, first, last):
+	while first < last:
+		p = partition(arr, first, last)
+		if (p - first) < (last - p):
+			quick_sort(arr, first, p - 1)
+			first = p
+		else:
+			quick_sort(arr, p, last)
+			last = p - 1
+
+def start_quick_sort(arr):
+	quick_sort(arr, 0, len(arr) - 1)
 
 a = [random.randrange(100) for x in range(0, 100)]
-quick_sort(a)
+start_quick_sort(a)
 print(a)
