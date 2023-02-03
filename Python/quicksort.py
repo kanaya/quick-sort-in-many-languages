82b752c8e4ca1ca0581d9c9c70e34df0db7fdf1f
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-03 17:44:39 +0900

    New algorithm.

--- Python/quicksort.py
+++ Python/quicksort.py
@@ -1,31 +1,14 @@
 import random
 
-def partition(arr, first, last):
-	pivot = arr[first]
-	while True:
-		while arr[first] < pivot:
-			first += 1
-		while pivot < arr[last]:
-			last -= 1
-		if first >= last:
-			return last + 1
-		arr[first], arr[last] = arr[last], arr[first]
-		first += 1
-		last -= 1
-
-def quick_sort(arr, first, last):
-	while first < last:
-		p = partition(arr, first, last)
-		if (p - first) < (last - p):
-			quick_sort(arr, first, p - 1)
-			first = p
-		else:
-			quick_sort(arr, p, last)
-			last = p - 1
-
-def start_quick_sort(arr):
-	quick_sort(arr, 0, len(arr) - 1)
+def quick_sort(arr):
+	if (len(arr) <= 1):
+		return arr
+	else:
+		pivot = arr[0]
+		arr_rest = arr[1:len(arr)]
+		left = list(filter(lambda x: x < pivot, arr_rest))
+		right = list(filter(lambda x: x >= pivot, arr_rest))
+		return quick_sort(left) + [pivot] + quick_sort(right)
 
 arr = [random.randrange(100) for x in range(0, 100)]
-start_quick_sort(arr)
-print(arr)
+print(quick_sort(arr))
