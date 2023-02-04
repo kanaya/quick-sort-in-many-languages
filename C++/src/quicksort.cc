12e0da5ba39eeffdef36b95da6552a9f76e9138d
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-04 11:40:27 +0900

    Now quicksort.cc uses iterators.

--- C++/src/quicksort.cc
+++ C++/src/quicksort.cc
@@ -4,49 +4,38 @@
 #include <vector>
 #include <random>
 
-template <typename T>
-std::size_t partition(std::vector<T> &arr, std::size_t begin, std::size_t end) {
-	T pivot = arr[begin];
-	std::size_t count = 0;
-
-	for (std::size_t i = begin + 1; i <= end; ++i) {
-		if (arr[i] <= pivot)
-			++count;
-	}
-
-	std::size_t it = begin + count;
-	std::swap(arr[it], arr[begin]);
-
-	std::size_t i = begin, j = end;
-
+template <typename Iterator, typename T = typename Iterator::value_type>
+auto partition(Iterator begin, Iterator end) {
+	T pivot = *begin;
+	auto it = std::find_if(begin, end, [pivot](T x) { return x <= pivot; });
+	std::swap(*it, *begin);  // really swaps?
+	auto i = begin, j = end;
 	while (i < it && j > it) {
-		while (arr[i] <= pivot) {
+		while (*i <= pivot) {
 			++i;
 		}
-		while (arr[j] > pivot) {
+		while (*j > pivot) {
 			--j;
 		}
 		if (i < it && j > it) {
-			std::swap(arr[i], arr[j]);
+			std::swap(*i, *j);  // really swaps?
 			++i;
-			--j;
+			++j;
 		}
 	}
 	return it;
 }
 
-template <typename T>
-void quick_sort(std::vector<T> &arr, std::size_t begin, std::size_t end) {
-	if (begin < end) {
-		std::size_t p = partition(arr, begin, end);
-		quick_sort(arr, begin, p - 1);
-		quick_sort(arr, p + 1, end);
+template <typename Iterator>
+void quick_sort(Iterator begin, Iterator end) {
+	if (begin >= end) {
+		return;
 	}
-}
-
-template <typename T>
-void start_quick_sort(std::vector<T> &arr) {
-	quick_sort(arr, 0, arr.size() - 1);
+	else {
+		auto p = partition(begin, end);
+		quick_sort(begin, p - 1);
+		quick_sort(p + 1, end);
+	}
 }
 
 int main() {
@@ -59,7 +48,7 @@ int main() {
 	std::generate(begin(arr), end(arr), gen);
 	std::size_t n = arr.size();
 
-	start_quick_sort(arr);
+	quick_sort(arr.begin(), arr.end());
 	for (int a: arr) {
 		std::cout << a << " ";
 	}
