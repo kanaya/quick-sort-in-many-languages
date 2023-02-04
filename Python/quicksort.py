9b8bdcdceda4459a02cde21054f018b85e9f150e
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-04 16:18:33 +0900

    Refactor.

--- Python/quicksort.py
+++ Python/quicksort.py
@@ -4,11 +4,11 @@ def quick_sort(arr):
 	if (len(arr) <= 1):
 		return arr
 	else:
-		pivot = arr[0]
-		arr_rest = arr[1:len(arr)]
-		left = list(filter(lambda x: x < pivot, arr_rest))
-		right = list(filter(lambda x: x >= pivot, arr_rest))
-		return quick_sort(left) + [pivot] + quick_sort(right)
+		x = arr[0]
+		xs = arr[1:len(arr)]
+		left = list(filter(lambda y: y < x, xs))
+		right = list(filter(lambda y: y >= x, xs))
+		return quick_sort(left) + [x] + quick_sort(right)
 
 arr = [random.randrange(100) for x in range(0, 100)]
 print(quick_sort(arr))
