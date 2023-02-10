17435fd950626489e3672f6171618cc57d8ad09c
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-10 16:26:52 +0900

    Started Rust version.

---
+++ Rust/quick_sort/target/debug/deps/quick_sort-3b1eb5c1520082cd.d
@@ -0,0 +1,5 @@
+/Users/kanaya/Home/quick-sort-in-many-languages/Rust/quick_sort/target/debug/deps/quick_sort-3b1eb5c1520082cd: src/main.rs
+
+/Users/kanaya/Home/quick-sort-in-many-languages/Rust/quick_sort/target/debug/deps/quick_sort-3b1eb5c1520082cd.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/quick_sort/target/debug/quick_sort.d
@@ -0,0 +1,1 @@
+/Users/kanaya/Home/quick-sort-in-many-languages/Rust/quick_sort/target/debug/quick_sort: /Users/kanaya/Home/quick-sort-in-many-languages/Rust/quick_sort/src/main.rs
