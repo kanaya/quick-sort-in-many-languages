a459518feaacfb258888fa47e4c4a8a1aa36d88b
Ichi Kanaya <kanaya@pineapple.cc>
2023-02-14 22:41:33 +0900

    refactor.

--- Rust/quick_sort/src/main.rs
+++ Rust/quick_sort/src/main.rs
@@ -11,14 +11,14 @@ fn main() {
 
 pub fn quick_sort<T: Ord>(arr: &mut [T]) {
     let len = arr.len();
-    _quick_sort(arr, 0, (len - 1) as isize);
+    quick_sort_impl(arr, 0, (len - 1) as isize);
 }
 
-fn _quick_sort<T: Ord>(arr: &mut [T], low: isize, high: isize) {
+fn quick_sort_impl<T: Ord>(arr: &mut [T], low: isize, high: isize) {
     if low < high {
         let p = partition(arr, low, high);
-        _quick_sort(arr, low, p - 1);
-        _quick_sort(arr, p + 1, high);
+        quick_sort_impl(arr, low, p - 1);
+        quick_sort_impl(arr, p + 1, high);
     }
 }
 
